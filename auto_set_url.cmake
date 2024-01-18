3f2bc731e771f207340016ee50fe04826ebb52e7
steffen.yount <steffen.yount@appdynamics.com>
2024-01-18 15:22:12 -0800

    Initial build scripts and blinky placeholder program

---
+++ auto_set_url.cmake
@@ -0,0 +1,5 @@
+set(SUPER_SCORPIO_URL_BASE "https://github.com/steffenyount/super-scorpio/tree")
+macro(auto_set_url TARGET)
+    file(RELATIVE_PATH URL_REL_PATH "${SUPER_SCORPIO_PATH}" "${CMAKE_CURRENT_LIST_DIR}")
+    pico_set_program_url(${TARGET} "${SUPER_SCORPIO_URL_BASE}/${URL_REL_PATH}")
+endmacro()
---
+++ main/main.c
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2024 Steffen Yount
+ */
+
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_usb_init();
+
+    const uint LED_PIN = 13;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
